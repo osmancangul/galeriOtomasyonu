d25b3ccd9b6054854d0be0ee9d2f69d9af9ed9f8
osmancangul <75526353+osmancangul@users.noreply.github.com>
2024-12-17 21:39:12 +0300

    Add files via upload
    C# ile OOP yapıcı metodlar kullanarak bir galeri otomasyonu geliştirdim. Console

---
+++ Galeri.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GaleriOtomasyonu
+{
+    public class Araba
+    {
+        public string plaka;
+        public string marka;
+        public string model;
+        public int yil;
+        public double fiyat;
+
+
+        public Araba(string _plaka, string _marka, string _model, int _yil, double _fiyat)
+        {
+            plaka = _plaka;
+            marka = _marka;
+            model = _model;
+            yil = _yil;
+            fiyat = _fiyat;
+        }
+
+        public void BilgiYazdir()
+        {
+            Console.WriteLine("Plaka: " + plaka);
+            Console.WriteLine("Marka: " + marka);
+            Console.WriteLine("Model: " + model);
+            Console.WriteLine("Yıl: " + yil);
+            Console.WriteLine("Fiyat: " + fiyat);
+        }
+    }
+
+    public class Galeri
+    {
+        public List<Araba> arabalar = new List<Araba>();
+
+        public void ArabaEkle(Araba araba)
+        {
+            arabalar.Add(araba);
+            Console.WriteLine($"Araba Eklendi: {araba.plaka}");
+        }
+
+        public void ArabaSil(string plaka)
+        {
+            Araba silinecekAraba = arabalar.Find(a => a.plaka == plaka);
+            if (silinecekAraba != null)
+            {
+                arabalar.Remove(silinecekAraba);
+                Console.WriteLine($"Araba Silindi: {plaka}");
+            }
+
+            else
+            {
+                Console.WriteLine($"Plaka {plaka} ile eşleşen araba bulunamadı.");
+            }
+        }
+
+        public void Listele()
+        {
+            Console.WriteLine("\n Galeri Listesi");
+            if (arabalar.Count == 0)
+            {
+                Console.WriteLine("Galeride Araç Yok.");
+                return;
+            }
+
+            foreach (var araba in arabalar)
+            {
+                araba.BilgiYazdir();
+            }
+        }
+    }
+
+}
+    
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaleriOtomasyonu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Galeri galeri = new Galeri();
+
+            while (true)
+            {
+
+                IslemSec();
+                string secim = Console.ReadLine();
+
+                switch (secim)
+                {
+                    case "1":
+                        Console.Write("Plaka: ");
+                        string _plaka = Console.ReadLine();
+                        Console.Write("Marka: ");
+                        string _marka = Console.ReadLine();
+                        Console.Write("Model: ");
+                        string _model = Console.ReadLine();
+                        Console.Write("Yıl: ");
+                        int _yil = int.Parse(Console.ReadLine());
+                        Console.Write("Fiyat: ");
+                        double _fiyat = double.Parse(Console.ReadLine());
+
+                        Araba araba = new Araba(_plaka, _marka, _model, _yil, _fiyat);
+                        galeri.ArabaEkle(araba);
+                        break;
+                    
+                    case "2":
+                        Console.WriteLine("Silinecek Arabanın Plakasını Giriniz: ");
+                        string silinecekAraba = Console.ReadLine();
+                        galeri.ArabaSil(silinecekAraba);
+                        break;
+                    
+                    case "3":
+                        galeri.Listele();
+                        break;
+                    
+                    case "4":
+                        Console.WriteLine("Programdan Çıkılıyor");
+                        break;
+                    
+                    default:
+                        Console.WriteLine("Geçersiz seçim... lütfen tekrar deneyin");
+                        break;
+                }
+            }
+
+
+
+        }
+
+        static void IslemSec()
+        {
+            Console.WriteLine("\nGaleri Otomasyonu");
+            Console.WriteLine("1. Araba Ekle");
+            Console.WriteLine("2. Araba Sil");
+            Console.WriteLine("3. Arabaları Listele");
+            Console.WriteLine("4. Çıkış");
+            Console.Write("Seçiminizi yapın: ");
+        }
+    }
+
+}
